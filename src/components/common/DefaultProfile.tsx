1bbc7282e11618de9ea769d9a9469b7048cd4cd6
hi-rachel <rachel.uiux@gmail.com>
2024-09-19 14:28:48 +0900

    style: adjust profile image sizing

--- src/components/common/DefaultProfile.tsx
+++ src/components/common/DefaultProfile.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import React from 'react';
 import { FaUserAstronaut } from 'react-icons/fa';
 
 interface DefaultProfileProps {
@@ -10,13 +9,13 @@ interface DefaultProfileProps {
 const DefaultProfile: React.FC<DefaultProfileProps> = ({ photoURL, size }) => {
 	return (
 		<div
-			className='rounded-full border-2 border-primary flex items-center justify-center'
-			style={{ width: size, height: size }}>
+			className='rounded-full border-2 border-primary flex items-center justify-center overflow-hidden'
+			style={{ width: `${size}px`, height: `${size}px` }}>
 			{photoURL ? (
 				<Image
+					className='object-cover'
 					src={photoURL}
 					alt='User Profile'
-					className='rounded-full'
 					width={size}
 					height={size}
 				/>
