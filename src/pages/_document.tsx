1fc170f933ad2d1f698482c98076ce391dfa74a2
hi-rachel <rachel.uiux@gmail.com>
2024-12-28 13:19:48 +0900

    feat: add meta description
    - change function to const for consistency

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
+const Document = () => {
 	return (
 		<Html lang='en'>
 			<Head>
 				<link rel='manifest' href='/manifest.json' />
 				<meta name='theme-color' content='#FF6B6B' />
+				<meta
+					name='description'
+					content='A private space to securely capture and share your most precious moments as a couple.'
+				/>
 				<link rel='apple-touch-icon' href='/icons/icon-192x192.png' />
 			</Head>
 			<body>
@@ -14,4 +18,6 @@ export default function Document() {
 			</body>
 		</Html>
 	);
-}
+};
+
+export default Document;
