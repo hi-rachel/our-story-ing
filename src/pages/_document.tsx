147c3b26276d495f2dce1fbce3512c612eb8c775
hi-rachel <rachel.uiux@gmail.com>
2024-12-28 14:08:07 +0900

    chore(manifest.json, _document.tsx): update theme_color, background_color
    - change meta content to korean

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,10 +5,10 @@ const Document = () => {
 		<Html lang='en'>
 			<Head>
 				<link rel='manifest' href='/manifest.json' />
-				<meta name='theme-color' content='#FF6B6B' />
+				<meta name='theme-color' content='#f472b6' />
 				<meta
 					name='description'
-					content='A private space to securely capture and share your most precious moments as a couple.'
+					content='커플들이 자신들의 여정을 공유할 수 있는 특별한 공간'
 				/>
 				<link rel='apple-touch-icon' href='/icons/icon-192x192.png' />
 			</Head>
