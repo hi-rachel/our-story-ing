7bd9b5ecabe0835e6a403bc649c706596e4aeff4
hi-rachel <rachel.uiux@gmail.com>
2025-01-31 00:17:51 +0900

    feat(meta): update title

--- src/components/common/meta/Meta.tsx
+++ src/components/common/meta/Meta.tsx
@@ -1,47 +1,34 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { useTranslation } from 'next-i18next';
 
 interface MetaProps {
-	ogTitle?: string;
-	ogDescription?: string;
-	ogImage?: string;
-	ogUrl?: string;
+	title?: string;
+	description?: string;
+	image?: string;
 }
 
 const Meta: React.FC<MetaProps> = ({
-	ogTitle = 'Welcome to Our Couple Web App ❤️ Ing',
-	ogDescription = 'Join this special platform for couples to chat, share memories, and connect!',
-	ogImage = 'https://our-story-ing.vercel.app/main.jpg',
-	ogUrl = 'https://our-story-ing.vercel.app',
+	title = 'Our Story Ing',
+	description = '커플들이 자신들의 여정을 공유할 수 있는 특별한 공간',
+	// image = 'https://our-story-ing.vercel.app/main.jpg',
 }) => {
 	const router = useRouter();
-	const { i18n } = useTranslation();
-
-	const defaultOgUrl =
-		ogUrl || `https://our-story-ing.vercel.app${router.asPath}`;
-
-	const localizedTitle =
-		i18n.language === 'ko'
-			? '커플 웹앱에 오신 것을 환영합니다 ❤️ Ing'
-			: ogTitle;
-
-	const localizedDescription =
-		i18n.language === 'ko'
-			? '채팅하고, 추억을 공유하며 소통할 수 있는 특별한 커플 플랫폼에 함께하세요!'
-			: ogDescription;
+	const baseUrl = 'https://our-story-ing.vercel.app';
 
 	return (
 		<Head>
-			<meta property='og:title' content={localizedTitle} />
-			<meta property='og:description' content={localizedDescription} />
-			<meta property='og:image' content={ogImage} />
-			<meta property='og:url' content={defaultOgUrl} />
+			<title>{title}</title>
+			<meta name='description' content={description} />
+
+			{/* Open Graph */}
+			<meta property='og:title' content={title} />
+			<meta property='og:description' content={description} />
+			{/* <meta property='og:image' content={image} /> */}
+			<meta property='og:url' content={`${baseUrl}${router.asPath}`} />
 			<meta property='og:type' content='website' />
-			<meta name='twitter:card' content='summary_large_image' />
-			<meta name='twitter:title' content={localizedTitle} />
-			<meta name='twitter:description' content={localizedDescription} />
-			<meta name='twitter:image' content={ogImage} />
+
+			{/* Twitter */}
+			{/* <meta name='twitter:card' content='summary_large_image' /> */}
 		</Head>
 	);
 };
