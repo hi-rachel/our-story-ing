018bf67746c416fe71fb40535fe19cf2f2a77742
hi-rachel <rachel.uiux@gmail.com>
2025-05-27 01:02:13 +0900

    style: 사진 테마 업데이트

--- src/constants/themes.ts
+++ src/constants/themes.ts
@@ -1,45 +1,124 @@
 export const themes = [
 	{
-		id: 'pink',
-		name: 'Pink',
-		path: '/images/ing-photo/theme-pink.png',
-		color: 'bg-[#FF9292]',
-	},
-	{
 		id: 'light-pink',
 		name: 'Light Pink',
-		path: '/images/ing-photo/theme-light-pink.png',
-		color: 'bg-[#FFC5C5]',
+		path: '/images/ing-photo/light-pink.png',
 	},
 	{
 		id: 'light-yellow',
 		name: 'Light Yellow',
-		path: '/images/ing-photo/theme-light-yellow.png',
-		color: 'bg-[#FFF7C1]',
+		path: '/images/ing-photo/light-yellow.png',
 	},
 	{
-		id: 'mint',
-		name: 'Mint',
-		path: '/images/ing-photo/theme-mint.png',
-		color: 'bg-[#C9FFC6]',
+		id: 'light-green',
+		name: 'Light Green',
+		path: '/images/ing-photo/light-green.png',
+	},
+	{
+		id: 'sky-blue',
+		name: 'Sky Blue',
+		path: '/images/ing-photo/sky-blue.png',
 	},
 
 	{
 		id: 'light-blue',
 		name: 'Light Blue',
-		path: '/images/ing-photo/theme-light-blue.png',
-		color: 'bg-[#A7DDFF]',
-	},
-	{
-		id: 'blue',
-		name: 'Blue',
-		path: '/images/ing-photo/theme-blue.png',
-		color: 'bg-[#48B6FF]',
+		path: '/images/ing-photo/light-blue.png',
 	},
 	{
 		id: 'light-purple',
 		name: 'Light Purple',
-		path: '/images/ing-photo/theme-light-purple.png',
-		color: 'bg-[#DCB8FF]',
+		path: '/images/ing-photo/light-purple.png',
+	},
+	{
+		id: 'white',
+		name: 'White',
+		path: '/images/ing-photo/white.png',
+	},
+	{
+		id: 'black',
+		name: 'Black',
+		path: '/images/ing-photo/black.png',
+		color: 'bg-[#000]',
+	},
+	{
+		id: 'cherry-blossom',
+		name: 'Cherry Blossom',
+		path: '/images/ing-photo/cherry-blossom.png',
+	},
+	{
+		id: 'sky-cherry-blossom',
+		name: 'Sky Cherry Blossom',
+		path: '/images/ing-photo/sky-cherry-blossom.png',
+	},
+	{
+		id: 'pink-rose',
+		name: 'Pink Rose',
+		path: '/images/ing-photo/pink-rose.png',
+	},
+	{
+		id: 'red-rose',
+		name: 'Red Rose',
+		path: '/images/ing-photo/red-rose.png',
+	},
+	{
+		id: 'pink-sky',
+		name: 'Pink Sky',
+		path: '/images/ing-photo/pink-sky.png',
+	},
+	{
+		id: 'summer-sunset',
+		name: 'Summer Sunset',
+		path: '/images/ing-photo/summer-sunset.png',
+	},
+	{
+		id: 'pink-marble',
+		name: 'Pink Marble',
+		path: '/images/ing-photo/pink-marble.png',
+	},
+	{
+		id: 'blue-pink-marble',
+		name: 'Blue Pink Marble',
+		path: '/images/ing-photo/blue-pink-marble.png',
+	},
+	{
+		id: 'sky-rainbow',
+		name: 'Sky Rainbow',
+		path: '/images/ing-photo/sky-rainbow.png',
+	},
+	{
+		id: 'star-night',
+		name: 'Star Night',
+		path: '/images/ing-photo/star-night.png',
+	},
+	{
+		id: 'star-night2',
+		name: 'Star Night2',
+		path: '/images/ing-photo/star-night2.png',
+	},
+	{
+		id: 'star-night3',
+		name: 'Star Night3',
+		path: '/images/ing-photo/star-night3.png',
+	},
+	{
+		id: 'cloud',
+		name: 'Cloud',
+		path: '/images/ing-photo/cloud.png',
+	},
+	{
+		id: 'sunset',
+		name: 'Sunset',
+		path: '/images/ing-photo/sunset.png',
+	},
+	{
+		id: 'sea-dolphin',
+		name: 'Sea Dolphin',
+		path: '/images/ing-photo/sea-dolphin.png',
+	},
+	{
+		id: 'wave',
+		name: 'Sea Dolphin',
+		path: '/images/ing-photo/wave.png',
 	},
 ];
